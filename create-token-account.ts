39f145e618bdac9acab27c8b934c62c1ffde097c
lige <li.ge@wondercv.com>
2025-02-24 17:59:32 +0800

    存储代币

---
+++ create-token-account.ts
@@ -0,0 +1,40 @@
+import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import "dotenv/config";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const user = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `🔑 Loaded our keypair securely, using an env file! Our public key is: ${user.publicKey.toBase58()}`,
+);
+
+// Substitute in your token mint account from create-token-mint.ts
+const tokenMintAccount = new PublicKey("DPsFaFQVcoSw7hdB3p16cBENRzFFaGAV8asbY4rS7AQ7");
+
+// Here we are making an associated token account for our own address, but we can
+// make an ATA on any other wallet in devnet!
+// const recipient = new PublicKey("SOMEONE_ELSES_DEVNET_ADDRESS");
+//const recipient = user.publicKey;
+const recipient = new PublicKey("CjnK7R6QWqvANHmiLvZFDsQPSJ9zktRtGkshBr3bWPUs");
+
+const tokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  user,
+  tokenMintAccount,
+  recipient,
+);
+
+console.log(`Token Account: ${tokenAccount.address.toBase58()}`);
+
+const link = getExplorerLink(
+  "address",
+  tokenAccount.address.toBase58(),
+  "devnet",
+);
+
+console.log(`✅ Created token Account: ${link}`);
