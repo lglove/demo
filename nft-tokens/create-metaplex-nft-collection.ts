7edda0ee5ca4e783440378884b944b4090423e45
lige <li.ge@wondercv.com>
2025-02-26 11:32:08 +0800

    添加图片

--- nft-tokens/create-metaplex-nft-collection.ts
+++ nft-tokens/create-metaplex-nft-collection.ts
@@ -34,3 +34,60 @@ await airdropIfRequired(
 );
 
 console.log("Loaded user:", user.publicKey.toBase58());
+
+const umi = createUmi(connection);
+
+// load keypair from local file system
+// See https://github.com/solana-developers/helpers?tab=readme-ov-file#get-a-keypair-from-a-keypair-file
+//const user = await getKeypairFromFile();
+
+// convert to umi compatible keypair
+const umiKeypair = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+
+// assigns a signer to our umi instance, and loads the MPL metadata program and Irys uploader plugins.
+umi
+  .use(keypairIdentity(umiKeypair))
+  .use(mplTokenMetadata())
+  .use(irysUploader());
+
+
+const collectionImagePath = path.resolve(__dirname, "collection.png");
+
+const buffer = await fs.readFile(collectionImagePath);
+let file = createGenericFile(buffer, collectionImagePath, {
+  contentType: "image/png",
+});
+const [image] = await umi.uploader.upload([file]);
+console.log("image uri:", image);
+
+// upload offchain json to Arweave using irys
+const uri = await umi.uploader.uploadJson({
+  name: "Bit Collection",
+  symbol: "BC",
+  description: "Bit Collection description",
+  image,
+});
+console.log("Collection offchain metadata URI:", uri);
+
+
+// generate mint keypair
+const collectionMint = generateSigner(umi);
+
+// create and mint NFT
+await createNft(umi, {
+  mint: collectionMint,
+  name: "Bit Collection",
+  uri,
+  updateAuthority: umi.identity.publicKey,
+  sellerFeeBasisPoints: percentAmount(0),
+  isCollection: true,
+}).sendAndConfirm(umi, { send: { commitment: "finalized" } });
+
+let explorerLink = getExplorerLink(
+  "address",
+  collectionMint.publicKey,
+  "devnet",
+);
+console.log(`Collection NFT:  ${explorerLink}`);
+console.log(`Collection NFT address is:`, collectionMint.publicKey);
+console.log("✅ Finished successfully!");
