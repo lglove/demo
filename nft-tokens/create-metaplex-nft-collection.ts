e3e84c81e8a0fdbe26c12c0f2ba8a76cae1f7e38
lige <li.ge@wondercv.com>
2025-02-26 11:20:39 +0800

    完善

---
+++ nft-tokens/create-metaplex-nft-collection.ts
@@ -0,0 +1,36 @@
+import {
+  createNft,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  createGenericFile,
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { promises as fs } from "fs";
+import * as path from "path";
+
+// create a new connection to Solana's devnet cluster
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// load keypair from local file system
+// assumes that the keypair is already generated using `solana-keygen new`
+const user = await getKeypairFromFile('solana.json');
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.1 * LAMPORTS_PER_SOL,
+);
+
+console.log("Loaded user:", user.publicKey.toBase58());
